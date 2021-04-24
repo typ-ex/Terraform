c0a5a293e521a271f4a2ad899c440e1761549328
Thomas Vu <the.criminal.element@gmail.com>
2021-04-24 14:24:06 -0500

    map example

--- learn-terraform-variables/main.tf
+++ learn-terraform-variables/main.tf
@@ -27,10 +27,7 @@ module "vpc" {
   enable_nat_gateway = true
   enable_vpn_gateway = var.enable_vpn_gateway
 
-  tags = {
-    project     = "project-alpha",
-    environment = "dev"
-  }
+  tags = var.resource_tags
 }
 
 module "app_security_group" {
