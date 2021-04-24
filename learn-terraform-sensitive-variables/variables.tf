8cbd3891cbd6e3ea78b701904ab8a66975c80c09
Thomas Vu <the.criminal.element@gmail.com>
2021-04-24 18:24:24 -0500

    Parameterizing creds to avoid plaintext

--- learn-terraform-sensitive-variables/variables.tf
+++ learn-terraform-sensitive-variables/variables.tf
@@ -51,3 +51,15 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "db_username" {
+  description = "Database administrator username"
+  type = string
+  sensitive = true
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type = string
+  sensitive = true
+}
