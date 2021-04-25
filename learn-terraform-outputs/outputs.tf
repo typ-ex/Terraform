fef4cffec0adb432ffe7a9d9fe0cd0bfc61ed485
Thomas Vu <the.criminal.element@gmail.com>
2021-04-24 23:22:38 -0500

    hiding database creds

--- learn-terraform-outputs/outputs.tf
+++ learn-terraform-outputs/outputs.tf
@@ -13,3 +13,15 @@ output "web_server_count" {
   description = "Number of web servers provisioned"
   value = length(module.ec2_instances.instance_ids)
 }
+
+output "db_username" {
+  description = "Database administrator username"
+  value = aws_db_instance.database.username
+  sensitive = true
+}
+
+output "db_password" {
+  description = "Database administrator password"
+  value = aws_db_instance.database.password
+  sensitive = true
+}
