9043bf44e4c55345f5965ed369cb6c622be242fa
Thomas Vu <the.criminal.element@gmail.com>
2021-04-24 21:35:41 -0500

    Custom output for creds.

--- learn-terraform-sensitive-variables/outputs.tf
+++ learn-terraform-sensitive-variables/outputs.tf
@@ -1,1 +1,5 @@
 # Output declarations
+output "db_connect_string" {
+  description = "MySQL database connection string"
+  value = "Server=${aws_db_instance.database.address}; Database=ExampleDB; Uid=${var.db_username}; Pwd=${var.db_password}"
+}
