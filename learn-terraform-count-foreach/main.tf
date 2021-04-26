d80821117190ba0f8ef6cc3ee069aafbb3b5b0a0
Thomas Vu <the.criminal.element@gmail.com>
2021-04-26 10:03:22 -0500

    Parameterizing/updating count of instances

--- learn-terraform-count-foreach/main.tf
+++ learn-terraform-count-foreach/main.tf
@@ -97,34 +97,13 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_instance" "app_a" {
+resource "aws_instance" "app" {
+  count = var.instances_per_subnet * length(module.vpc.private_subnets)
+
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
-  subnet_id              = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [module.app_security_group.this_security_group_id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
-  tags = {
-    Terraform   = "true"
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_instance" "app_b" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-
-  subnet_id              = module.vpc.private_subnets[1]
+  subnet_id              = module.vpc.private_subnets[count.index % length(module.pvc.prviate_subnets)]
   vpc_security_group_ids = [module.app_security_group.this_security_group_id]
 
   user_data = <<-EOF
