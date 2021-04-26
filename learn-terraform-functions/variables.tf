6d140f2ed31f7c8230e974c7a0b24412003ccb6b
Thomas Vu <the.criminal.element@gmail.com>
2021-04-26 16:20:27 -0500

    updating variables

--- learn-terraform-functions/variables.tf
+++ learn-terraform-functions/variables.tf
@@ -31,8 +31,8 @@ variable "user_department" {
 variable "aws_amis" {
   type = map(any)
   default = {
-    "us-east-1" = "ami=0739f8cdb239fe9ae"
-    "us-west-2" = "ami=008b09448b998a562"
-    "us-east-2" = "ami=0ebc8f6f580a04647"
+    "us-east-1" = "ami-0739f8cdb239fe9ae"
+    "us-west-2" = "ami-008b09448b998a562"
+    "us-east-2" = "ami-0ebc8f6f580a04647"
   }
 }
