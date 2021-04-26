ea69f313f490f52a0643dde6c254cfdc42e99d1a
Thomas Vu <the.criminal.element@gmail.com>
2021-04-26 17:14:04 -0500

    add sg outputs

--- learn-terraform-functions/outputs.tf
+++ learn-terraform-functions/outputs.tf
@@ -9,3 +9,11 @@ output "web_public_ip" {
 output "ami_value" {
   value = lookup(var.aws_amis, var.aws_region)
 }
+
+output "web_security_group_ids" {
+  value = concat([aws_security_group.sg_22.id, aws_security_group.sg_8080.id])
+}
+
+output "web_security_group_ids" {
+  value = concat([aws_security_group.sg_22.id, aws_security_group.sg_8080.id])
+}
