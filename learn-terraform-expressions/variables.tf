0917556a05fe6d14c708d6dd5fdc8c416de95b14
Thomas Vu <the.criminal.element@gmail.com>
2021-04-26 19:49:39 -0500

    Initial expressions tutorial upload

---
+++ learn-terraform-expressions/variables.tf
@@ -0,0 +1,24 @@
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "172.16.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "172.16.10.0/24"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy your instance"
+  default     = "us-east-2"
+}
+
+variable "name" {
+  description = "The username assigned to the infrastructure"
+  default     = "terraform"
+}
+
+variable "team" {
+  description = "The team responsible for the infrastructure"
+  default     = "hashicorp"
+}
+
