d4d8fd1dabb0572e4ca259c914cffe37a6a858e4
Thomas Vu <the.criminal.element@gmail.com>
2021-05-03 11:00:59 -0500

    New source link

--- learn-private-module-root/main.tf
+++ learn-private-module-root/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/thrillho-first/s3-webapp/aws"
+  source  = "app.terraform.io/thrillho-first/terraform/thrillho-first/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
