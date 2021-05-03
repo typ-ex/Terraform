7cf2ec83160fee8aac9913d669801be9966f86f5
Thomas Vu <the.criminal.element@gmail.com>
2021-05-03 11:08:34 -0500

    Updated source 3

--- learn-private-module-root/main.tf
+++ learn-private-module-root/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/thrillho-first/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
   version = "1.0.0"
 }
