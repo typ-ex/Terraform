63301ef89483db662590e4b7d9d6d8d29b460803
Thomas Vu <the.criminal.element@gmail.com>
2021-05-10 11:54:02 -0500

    Migrating state to cloud

--- learn-state-migration/main.tf
+++ learn-state-migration/main.tf
@@ -1,14 +1,14 @@
 ## Terraform configuration
-terraform{
+terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.0.1"
     }
   }
   required_version = "~> 0.14"
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "thrillho-first"
 
     workspaces {
