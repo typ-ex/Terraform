f7b876e64dc77c162c87c4696c18486b6691b991
Thomas Vu <the.criminal.element@gmail.com>
2021-05-12 09:11:56 -0500

    Initial upload drift management tutorial

--- learn-terraform-resource-targeting/main.tf
+++ learn-terraform-resource-targeting/main.tf
@@ -34,7 +34,6 @@ resource "random_pet" "object_names" {
 
   length    = 5
   separator = "_"
-  prefix    = "learning"
 }
 
 resource "aws_s3_bucket_object" "objects" {
@@ -43,6 +42,6 @@ resource "aws_s3_bucket_object" "objects" {
   acl          = "public-read"
   key          = "${random_pet.object_names[count.index].id}.txt"
   bucket       = module.s3_bucket.s3_bucket_id
-  content      = "Example object #${count.index}"
+  content      = "Bucket object #${count.index}"
   content_type = "text/plain"
 }
